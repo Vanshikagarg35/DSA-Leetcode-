37fe0e6ad7b028cd0e9e9b6e37f12b44dbdfd09d
Vanshika garg <vnamratagarg35@gmail.com>
2026-03-16 13:22:25 +0530

    Time: 4 ms (31.56%), Space: 8.6 MB (37.38%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int y = x;
-        string palin = to_string(y);
-        reverse(palin.begin(),palin.end());
-        return to_string(x)==(palin);
-
+        int temp =x;
+        long long rev_num = 0;
+        while(temp>0){
+            int n = temp%10;
+            rev_num = (rev_num*10) + n;
+            temp=temp/10;
+        }
+        return (rev_num == x);
     }
 };
