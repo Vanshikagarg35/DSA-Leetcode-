86cb7d9d0d178451a0b38c07293acc653851a5e6
Vanshika garg <vnamratagarg35@gmail.com>
2026-03-27 00:25:44 +0530

    Time: 0 ms (100%), Space: 35.9 MB (65.4%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,43 +1,16 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<int> result;
         int start = 0;
-        int end = arr.size() - 1;
-        
-        while(start <= end){
+        int end = arr.size() - k;  
+        while (start < end) {
             int mid = start + (end - start) / 2;
-            if(arr[mid] < x) start = mid + 1;
-            else end = mid - 1;
-        }
-        
-        int i = start - 1;
-        int j = start;
-        int count = 0;
-
-        while(count < k) {
-            if (i < 0) {
-                result.push_back(arr[j]);
-                j++;
-            } 
-            else if (j >= arr.size()) {
-                result.push_back(arr[i]);
-                i--;
-            } 
-           
-            else {
-                if (x - arr[i] <= arr[j] - x) {
-                    result.push_back(arr[i]);
-                    i--;
-                } else {
-                    result.push_back(arr[j]);
-                    j++;
-                }
+            if (x - arr[mid] > arr[mid + k] - x) {
+                start = mid + 1;
+            } else {
+                end = mid;
             }
-            count++;
         }
-        
-        sort(result.begin(), result.end());
-        return result;
+        return vector<int>(arr.begin() + start, arr.begin() + start + k);
     }
 };
