3f966aa52582c21ac3fe2f4e69fbbcf28a792f9a
Vanshika garg <vnamratagarg35@gmail.com>
2026-03-30 12:39:58 +0530

    Time: 0 ms (100%), Space: 25.2 MB (76.15%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int>result;
+        for(int i=0; i<nums.size(); i++){
+            int index = abs(nums[i])-1;
+            if(nums[index]>0) nums[index] = -nums[index];
+            else{
+                result.push_back(abs(nums[i]));
+            }
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]>0) result.push_back(i+1);
+        }
+        return result;
+    }
+};
