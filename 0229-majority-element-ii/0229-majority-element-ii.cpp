fad36706641bbd7e8178621ac3d8bea952b019df
Vanshika garg <vnamratagarg35@gmail.com>
2026-04-02 17:05:56 +0530

    Time: 0 ms (100%), Space: 23.5 MB (96.51%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cand1 = 0, cand2 = 0;
-        int count1 = 0, count2 = 0;
-        for (int num : nums) {
-            if (num == cand1) {
+        int count1 = 0;
+        int count2 = 0;
+        int n = nums.size();
+        vector<int> ans;
+        int candidate1 = INT_MIN;
+        int candidate2 = INT_MIN;
+
+        if(n == 0) return ans;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == candidate1) {
                 count1++;
-            } else if (num == cand2) {
+            }
+            else if(nums[i] == candidate2) {
                 count2++;
-            } else if (count1 == 0) {
-                cand1 = num;
+            }
+            else if(count1 == 0) {
+                candidate1 = nums[i];
                 count1 = 1;
-            } else if (count2 == 0) {
-                cand2 = num;
+            }
+            else if(count2 == 0) {
+                candidate2 = nums[i];
                 count2 = 1;
-            } else {
-  
+            }
+            else {
                 count1--;
                 count2--;
             }
@@ -23,16 +33,15 @@ public:
 
         count1 = 0;
         count2 = 0;
-        for (int num : nums) {
-            if (num == cand1) count1++;
-            else if (num == cand2) count2++;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == candidate1) count1++;
+            else if(nums[i] == candidate2) count2++;
         }
+
+        if(count1 > n / 3) ans.push_back(candidate1);
+        if(count2 > n / 3) ans.push_back(candidate2);
         
-        vector<int> result;
-        int n = nums.size();
-        if (count1 > n / 3) result.push_back(cand1);
-        if (count2 > n / 3) result.push_back(cand2);
-        
-        return result;
+        return ans;
     }
 };
