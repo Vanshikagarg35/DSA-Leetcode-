f6e45a25985bae4f87a1f74f6c604efbb15c9980
Vanshika garg <vnamratagarg35@gmail.com>
2026-04-19 09:46:10 +0530

    Time: 191 ms (83.24%), Space: 10.4 MB (82.14%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool backtrack(vector<vector<char>>& board, string& word, int i, int j, int idx) {
+        if (idx == word.length()) {
+            return true;
+        }
+        
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[idx]) {
+            return false;
+        }
+        
+        char temp = board[i][j];
+        board[i][j] = '*'; 
+        
+        bool found = backtrack(board, word, i + 1, j, idx + 1) ||  
+                     backtrack(board, word, i - 1, j, idx + 1) ||  
+                     backtrack(board, word, i, j + 1, idx + 1) ||  
+                     backtrack(board, word, i, j - 1, idx + 1);    
+                     
+        board[i][j] = temp;
+        
+        return found;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0]) {
+                    if (backtrack(board, word, i, j, 0)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+};
