3eb42b70b320d53416a8067a83103deba546fc40
Vanshika garg <vnamratagarg35@gmail.com>
2026-06-16 21:23:17 +0530

    Time: 477 ms (21.21%), Space: 296.6 MB (6%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-    int LCS(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        
-        vector<vector<int>> t(m+1, vector<int>(n+1));
-
-        for(int j = 0; j<n; j++) {
-            t[0][j] = 0;
-        }
-        for(int i = 0; i<n; i++) {
-            t[i][0] = 0;
-        }
-        
-        for(int i = 1; i<m+1; i++) {
-            for(int j = 1; j<n+1; j++) {
-                if(nums1[i-1] == nums2[j-1])
-                    t[i][j] = 1 + t[i-1][j-1];
-                else
-                    t[i][j] = max(t[i-1][j], t[i][j-1]);
-            }
-        }
-        
-        return t[m][n];
-    }
     int lengthOfLIS(vector<int>& nums) {
-      
-        set<int> st(begin(nums), end(nums));
+        int n = nums.size();
+        vector<vector<int>> t(n + 1, vector<int>(n + 1, -1));
+        return solve(nums, t, -1, 0, n);
+    }
+    
+    int solve(vector<int>& nums, vector<vector<int>>& t, int previdx, int curridx, int n){
+        if(curridx == n) return 0;
         
-        vector<int> nums2(begin(st), end(st));
+        if(previdx != -1 && t[previdx][curridx] != -1){
+            return t[previdx][curridx];
+        }
         
-        return LCS(nums, nums2);
+        int taken=0;
+        if(previdx == -1 || nums[curridx] > nums[previdx]){
+            taken = 1 + solve(nums, t, curridx, curridx + 1, n);
+        }
         
+        int notaken = solve(nums, t, previdx, curridx + 1, n);
+        
+        if(previdx != -1){
+            t[previdx][curridx] = max(taken, notaken);
+        }
+        
+        return max(taken, notaken);
     }
 };
