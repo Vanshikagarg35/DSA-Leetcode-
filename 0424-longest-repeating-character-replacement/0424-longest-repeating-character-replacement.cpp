f25ff1699980d98ae2d484a0ad3b174aa2e77be3
Vanshika garg <vnamratagarg35@gmail.com>
2026-07-06 13:51:08 +0530

    Time: 0 ms (100%), Space: 10.7 MB (93.25%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,53 +1,23 @@
-#include <unordered_set>
-#include <algorithm>
-#include <string>
-
-using namespace std;
-
 class Solution {
 public:
-    // Ek generic function jo kisi bhi 'target' character ke liye longest substring nikalega
-    int charResp(string &s, int k, char target){
+    int characterReplacement(string s, int k) {
+        vector<int> count(26, 0);
+        int maxf = 0;
         int i = 0;
-        int j = 0;
-        int Kcount = 0;
-        int count = 0;
         int maxC = 0;
         
-        while(j < s.length()){
-        
-            if(Kcount == k && s[j] != target){
-                while(s[i] == target){
-                    count--;
-                    i++;
-                }
-                count--;
-                Kcount--;
+        for (int j = 0; j < s.length(); j++) {
+            count[s[j] - 'A']++;
+            maxf = max(maxf, count[s[j] - 'A']);
+            
+            while ((j - i + 1) - maxf > k) {
+                count[s[i] - 'A']--;
                 i++;
             }
-            else{
-                if(s[j] != target && Kcount < k) Kcount++;
-                count++;
-                maxC = max(maxC, count);
-                j++;
-            }
+            
+            maxC = max(maxC, j - i + 1);
         }
+        
         return maxC;
-    }
-
-    int characterReplacement(string s, int k) {
-        unordered_set<char> uniqueChars;
-        for(int i = 0; i < s.length(); i++){
-            uniqueChars.insert(s[i]);
-        }
-        
-        int longestsubstr = 0;
-
-        for(char target : uniqueChars){
-            int currentMax = charResp(s, k, target);
-            longestsubstr = max(longestsubstr, currentMax);
-        }
-        
-        return longestsubstr;
     }
 };
