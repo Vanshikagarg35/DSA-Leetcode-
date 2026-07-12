23e16ef8760e14d4bdb337b914d566b6963d08df
Vanshika garg <vnamratagarg35@gmail.com>
2026-07-12 19:10:24 +0530

    Time: 6 ms (30.65%), Space: 127.5 MB (18.65%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,34 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if(head == NULL || head->next == NULL) return true;
-
-        ListNode *slow = head;
-        ListNode *fast = head;
-        while(fast != NULL && fast->next != NULL){
-            slow = slow->next;
-            fast = fast->next->next;
+        stack<int>s;
+        ListNode* temp = head;
+        while(temp!=NULL){
+            s.push(temp->val);
+            temp=temp->next;
         }
-
-        ListNode *prev = NULL;
-        ListNode *curr = slow;
-        while(curr != NULL){
-            ListNode *fut = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = fut;
+        temp=head;
+        while(temp!=NULL){
+            if(s.top()!=temp->val) return false;
+            s.pop();
+            temp=temp->next;
         }
-
-        ListNode *comp1 = head;
-        ListNode *comp2 = prev;
-        while(comp2 != NULL){
-            if(comp1->val != comp2->val){
-                return false; 
-            }
-            comp1 = comp1->next;
-            comp2 = comp2->next;
-        }
-        
         return true;
     }
 };
